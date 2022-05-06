9862c48eb1940df932768754278c6508b67dcc1a
Ashwin Kumar <its.ashwin.kumar@gmail.com>
2022-05-06 09:53:18 +0530

    chore: Implemented ScriptingDefinesManager to manage plugin specific define symbols chore: Implemented CollectionUtility to include basic extension for lists/dict

---
+++ Editor/CoreLibrary/Misc/ScriptingDefinesManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using VoxelBusters.CoreLibrary;
+
+namespace VoxelBusters.CoreLibrary.Editor
+{
+    public static class ScriptingDefinesManager
+    {
+        #region Static fields
+
+        private     static      Dictionary<BuildTargetGroup, List<string>>      s_addDefinesCollection;
+
+        private     static      Dictionary<BuildTargetGroup, List<string>>      s_removeDefinesCollection;
+
+        private     static      BuildTargetGroup[]                              s_supportedTargetGroups;
+
+
+        #endregion
+
+        #region Static methods
+
+        public static void AddDefine(string define, params BuildTargetGroup[] targetGroups)
+        {
+            EnsureInitialized();
+
+            AddDefineToCollection(definesCollection: s_addDefinesCollection, define: define, targetGroups: targetGroups ?? s_supportedTargetGroups);
+            UpdateDefineSymbolsDelayed();
+        }
+
+        public static void RemoveDefine(string define, params BuildTargetGroup[] targetGroups)
+        {
+            EnsureInitialized();
+
+            AddDefineToCollection(definesCollection: s_removeDefinesCollection, define: define, targetGroups: targetGroups ?? s_supportedTargetGroups);
+            UpdateDefineSymbolsDelayed();
+        }
+
+        private static void EnsureInitialized()
+        {
+            if (s_addDefinesCollection != null) return;
+
+            // set properties
+            s_addDefinesCollection      = new Dictionary<BuildTargetGroup, List<string>>();
+            s_removeDefinesCollection   = new Dictionary<BuildTargetGroup, List<string>>();
+
+            var     targetValues        = System.Enum.GetValues(typeof(BuildTargetGroup));
+            var     iter                = 0;
+            s_supportedTargetGroups     = new BuildTargetGroup[targetValues.Length];
+            foreach (var item in System.Enum.GetValues(typeof(BuildTargetGroup)))
+            {
+                s_supportedTargetGroups[iter++] = (BuildTargetGroup)item;
+            }
+        }
+
+        private static void AddDefineToCollection(Dictionary<BuildTargetGroup, List<string>> definesCollection, string define, BuildTargetGroup[] targetGroups)
+        {
+            // add define symbol for all the specified target groups
+            foreach (var group in targetGroups)
+            {
+                if (!definesCollection.TryGetValue(group, out List<string> groupDefines))
+                {
+                    var     newDefines  = new List<string>();
+                    definesCollection.Add(group, newDefines);
+
+                    groupDefines        = newDefines;
+                }
+
+                groupDefines.AddUnique(define);
+            }
+        }
+
+        private static void UpdateDefineSymbolsDelayed()
+        {
+            EditorApplication.delayCall -= UpdateDefineSymbols;
+            EditorApplication.delayCall += UpdateDefineSymbols;
+        }
+
+        private static void UpdateDefineSymbols()
+        {
+            try
+            {
+                EnsureInitialized();
+                AssetDatabase.StartAssetEditing();
+
+                foreach (var targetGroup in s_supportedTargetGroups)
+                {
+                    var     existingDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(targetGroup).Split(';');
+                    var     updatedDefines  = new List<string>(existingDefines);
+                    bool    isModified      = false;
+                    if (s_addDefinesCollection.TryGetValue(targetGroup, out List<string> addDefines))
+                    {
+                        foreach (var define in addDefines)
+                        {
+                            isModified     |= updatedDefines.AddUnique(define);
+                        }
+                    }
+                    if (s_removeDefinesCollection.TryGetValue(targetGroup, out List<string> removeDefines))
+                    {
+                        foreach (var define in removeDefines)
+                        {
+                            isModified     |= updatedDefines.Remove(define);
+                        }
+                    }
+
+                    // set values if there are modifications
+                    if (isModified)
+                    {
+                        PlayerSettings.SetScriptingDefineSymbolsForGroup(targetGroup, string.Join(";", updatedDefines.ToArray()));
+                    }
+                }
+            }
+            finally
+            {
+                AssetDatabase.StopAssetEditing();
+
+                // reset properties
+                s_addDefinesCollection.Clear();
+                s_removeDefinesCollection.Clear();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Runtime/CoreLibrary/Utilities/CollectionUtility.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VoxelBusters.CoreLibrary
+{
+    public static class CollectionUtility
+    {
+        #region Extension methods
+
+        public static bool AddUnique<T>(this List<T> list, T item)
+        {
+            if (null == list) return false;
+
+            if (!list.Contains(item))
+            {
+                list.Add(item);
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
