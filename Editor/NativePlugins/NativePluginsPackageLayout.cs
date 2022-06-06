94c0c6667f434408e19b7283a3f7b1c93f3c27d7
Ashwin Kumar <its.ashwin.kumar@gmail.com>
2022-06-06 17:47:47 +0530

    fix: Updated package layout path references for ExtrasPath.

--- Editor/NativePlugins/NativePluginsPackageLayout.cs
+++ Editor/NativePlugins/NativePluginsPackageLayout.cs
@@ -6,7 +6,7 @@ namespace VoxelBusters.CoreLibrary.Editor.NativePlugins
 {
     public static class NativePluginsPackageLayout
     {
-        public static string ExtrasPath { get { return "Assets/Plugins/VoxelBusters/NativePlugins"; } }
+        public static string ExtrasPath { get { return "Assets/Plugins/VoxelBusters/CoreLibrary"; } }
 
         public static string IosPluginPath { get { return ExtrasPath + "/Plugins/iOS"; } }
     }
