dba4bca5497a9339218030dd1c69cf233460df36
Ashwin Kumar <its.ashwin.kumar@gmail.com>
2022-06-24 12:35:59 +0530

    chore: Renamed class KeyValueStore as KeyValueDataStore

--- Runtime/CoreLibrary/DataModels/KeyValueDataStore.cs
+++ Runtime/CoreLibrary/DataModels/KeyValueDataStore.cs
@@ -5,7 +5,10 @@ using VoxelBusters.CoreLibrary;
 
 namespace VoxelBusters.CoreLibrary
 {
-    public class KeyValueStore
+    /// <summary>
+    /// Object represents a container to store key-value pairs.
+    /// </summary>
+    public class KeyValueDataStore
     {
         #region Fields
 
@@ -17,7 +20,11 @@ namespace VoxelBusters.CoreLibrary
 
         #region Constructors
 
-        public KeyValueStore(string savePath)
+        /// <summary>
+        /// Create a new instance.
+        /// </summary>
+        /// <param name="savePath">The file path to save contents.</param>
+        public KeyValueDataStore(string savePath)
         {
             // set properties
             m_dataCollection    = LoadDataFromPath(savePath) ?? new Dictionary<string, string>();
@@ -28,6 +35,12 @@ namespace VoxelBusters.CoreLibrary
 
         #region Get value methods
 
+        /// <summary>
+        /// Returns the boolean value associated with the specified key.
+        /// </summary>
+        /// <param name="key">A string used to identify the value stored in the data store.</param>
+        /// <param name="defaultValue">Default value.</param>
+        /// <returns></returns>
         public bool GetBool(string key, bool defaultValue = default)
         {
             if (m_dataCollection.TryGetValue(key, out string value))
@@ -37,6 +50,12 @@ namespace VoxelBusters.CoreLibrary
             return defaultValue;
         }
 
+        /// <summary>
+        /// Returns the long value associated with the specified key.
+        /// </summary>
+        /// <param name="key">A string used to identify the value stored in the data store.</param>
+        /// <param name="defaultValue">Default value.</param>
+        /// <returns></returns>
         public long GetLong(string key, long defaultValue = default)
         {
             if (m_dataCollection.TryGetValue(key, out string value))
@@ -46,6 +65,12 @@ namespace VoxelBusters.CoreLibrary
             return defaultValue;
         }
 
+        /// <summary>
+        /// Returns the double value associated with the specified key.
+        /// </summary>
+        /// <param name="key">A string used to identify the value stored in the data store.</param>
+        /// <param name="defaultValue">Default value.</param>
+        /// <returns></returns>
         public double GetDouble(string key, double defaultValue = default)
         {
             if (m_dataCollection.TryGetValue(key, out string value))
@@ -55,6 +80,12 @@ namespace VoxelBusters.CoreLibrary
             return defaultValue;
         }
 
+        /// <summary>
+        /// Returns the string value associated with the specified key.
+        /// </summary>
+        /// <param name="key">A string used to identify the value stored in the data store.</param>
+        /// <param name="defaultValue">Default value.</param>
+        /// <returns></returns>
         public string GetString(string key, string defaultValue = default)
         {
             if (m_dataCollection.TryGetValue(key, out string value))
@@ -64,6 +95,12 @@ namespace VoxelBusters.CoreLibrary
             return defaultValue;
         }
 
+        /// <summary>
+        /// Returns the byte array object associated with the specified key.      
+        /// </summary>
+        /// <param name="key">A string used to identify the value stored in the data store.</param>
+        /// <param name="defaultValue">Default value.</param>
+        /// <returns></returns>
         public byte[] GetByteArray(string key, byte[] defaultValue = default)
         {
             if (m_dataCollection.TryGetValue(key, out string value))
@@ -77,30 +114,55 @@ namespace VoxelBusters.CoreLibrary
 
         #region Set value methods
 
+        /// <summary>
+        /// Sets a boolean value for the specified key in the data store.
+        /// </summary>
+        /// <param name="key">The key under which to store the value.</param>
+        /// <param name="value">The boolean value to store.</param>
         public void SetBool(string key, bool value)
         {
             // save value
             m_dataCollection[key]   = value.ToString();
         }
 
+        /// <summary>
+        /// Sets a long value for the specified key in the data store.
+        /// </summary>
+        /// <param name="key">The key under which to store the value.</param>
+        /// <param name="value">The long value to store.</param>
         public void SetLong(string key, long value)
         {
             // save value
             m_dataCollection[key]   = value.ToString();
         }
 
+        /// <summary>
+        /// Sets a double value for the specified key in the data store.
+        /// </summary>
+        /// <param name="key">The key under which to store the value.</param>
+        /// <param name="value">The double value to store.</param>
         public void SetDouble(string key, double value)
         {
             // save value
             m_dataCollection[key]   = value.ToString();
         }
 
+        /// <summary>
+        /// Sets a string value for the specified key in the data store.
+        /// </summary>
+        /// <param name="key">The key under which to store the value.</param>
+        /// <param name="value">The string value to store.</param>
         public void SetString(string key, string value)
         {
             // save value
             m_dataCollection[key]   = value;
         }
 
+        /// <summary>
+        /// Sets a byte array value for the specified key in the data store.
+        /// </summary>
+        /// <param name="key">The key under which to store the value.</param>
+        /// <param name="value">The long value to store.</param>
         public void SetByteArray(string key, byte[] value)
         {
             // save value
@@ -111,18 +173,28 @@ namespace VoxelBusters.CoreLibrary
 
         #region Misc methods
 
+        /// <summary>
+        /// Explicitly synchronizes in-memory data with those stored on disk.
+        /// </summary>
         public void Synchronize()
         {
             string  jsonContent = ExternalServiceProvider.JsonServiceProvider.ToJson(m_dataCollection);
             IOServices.CreateFile(m_savePath, jsonContent);
         }
 
+        /// <summary>
+        /// Removes all the entries from the data store.
+        /// </summary>
         public void Clear()
         {
             m_dataCollection.Clear();
             Synchronize();
         }
 
+        /// <summary>
+        /// Removes the value associated with the specified key from the data store.
+        /// </summary>
+        /// <param name="key">The key corresponding to the value you want to remove.</param>
         public bool RemoveKey(string key)
         {
             return m_dataCollection.Remove(key);
