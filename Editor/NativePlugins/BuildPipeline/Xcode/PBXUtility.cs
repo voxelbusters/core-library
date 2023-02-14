ccc69078e69caa26f5ce44114de134a39ea17e9c
Ashwin Kumar <its.ashwin.kumar@gmail.com>
2023-02-14 18:28:25 +0530

    fix: Entitlement filename updated to  use standard name based on product name

--- Editor/NativePlugins/BuildPipeline/Xcode/PBXUtility.cs
+++ Editor/NativePlugins/BuildPipeline/Xcode/PBXUtility.cs
@@ -10,8 +10,7 @@ namespace VoxelBusters.CoreLibrary.Editor.NativePlugins.Build.Xcode
     {
         public static ProjectCapabilityManager CreateDefaultProjectCapabilityManager(PBXProject project, string projectPath, string targetGuid)
         {
-            string  productName = project.GetBuildPropertyForAnyConfig(targetGuid, "PRODUCT_NAME");
-            string  fileName    = $"{productName}.entitlements";
+            string  fileName    = $"{Application.productName}.entitlements";
 #if UNITY_2019_3_OR_NEWER
             return new ProjectCapabilityManager(projectPath, fileName, null, targetGuid);
 #else
