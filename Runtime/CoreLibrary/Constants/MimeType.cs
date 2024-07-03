de0f503f9bdaaed687a6fb4ae4078211a3c0b6c2
Ayyappa <ayyappa.99@gmail.com>
2024-07-04 04:34:44 +0530

    chore: Added additional mime types

--- Runtime/CoreLibrary/Constants/MimeType.cs
+++ Runtime/CoreLibrary/Constants/MimeType.cs
@@ -15,6 +15,9 @@ namespace VoxelBusters.CoreLibrary
     {
         #region Constants
 
+        /// <summary> The MIME value used to determine any file. (Readonly)</summary>
+        public  const   string      kAny          = "*/*";
+
         /// <summary> The MIME value used to determine plain text file. (Readonly)</summary>
         public  const   string      kPlainText          = "text/plain";
 
@@ -23,6 +26,9 @@ namespace VoxelBusters.CoreLibrary
 
         /// <summary> The MIME value used to determine javascript content. (Readonly)</summary>
         public  const   string      kJavaScriptText     = "text/javascript";
+
+        /// <summary> The MIME value used to determine all image files. (Readonly)</summary>
+        public  const   string      kAllImages          = "image/*";
 
         /// <summary> The MIME value used to determine jpg image file. (Readonly)</summary>
         public  const   string      kJPGImage           = "image/jpeg";
@@ -36,8 +42,15 @@ namespace VoxelBusters.CoreLibrary
         /// <summary> The MIME value used to determine Adobe® PDF documents. (Readonly)</summary>
         public  const   string      kPDF                = "application/pdf";
 
+
+        /// <summary> The MIME value used to determine all video files. (Readonly)</summary>
+        public  const   string      kAllVideos          = "video/*";
+
         /// <summary> The MIME value used to determine mp4 video. (Readonly)</summary>
         public  const   string      kMP4Video           = "video/mp4";
+
+        /// <summary> The MIME value used to determine all audio files. (Readonly)</summary>
+        public  const   string      kAllAudio          = "audio/*";
 
         #endregion
 
@@ -81,6 +94,57 @@ namespace VoxelBusters.CoreLibrary
             }
 
             DebugLogger.LogWarning(CoreLibraryDomain.Default, $"Unknown MIME type for extension: {extension}");
+            return kPlainText;
+        }
+
+        public static string GetExtensionForType(string mimeType)
+        {
+            if (string.Equals(mimeType, kPlainText, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return "txt";
+            }
+            else if (string.Equals(mimeType, kHtmlText, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return "html";
+            }
+            else if (string.Equals(mimeType, kJavaScriptText, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return "js";
+            }
+            else if (string.Equals(mimeType, kJPGImage, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return "jpg";
+            }
+            else if (string.Equals(mimeType, kPNGImage, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return "png";
+            }
+            else if (string.Equals(mimeType, kGIFImage, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return "gif";
+            }
+            else if (string.Equals(mimeType, kPDF, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return "pdf";
+            }
+            else if (string.Equals(mimeType, kMP4Video, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return "mp4";
+            }
+            else if (string.Equals(mimeType, kAllImages, StringComparison.InvariantCultureIgnoreCase)) //Defaults to png
+            {
+                return "png";
+            }
+            else if (string.Equals(mimeType, kAllVideos, StringComparison.InvariantCultureIgnoreCase)) //Defaults to mp4
+            {
+                return "mp4";
+            }
+            else if (string.Equals(mimeType, kAllAudio, StringComparison.InvariantCultureIgnoreCase)) //Defaults to wav
+            {
+                return "wav";
+            }
+
+            DebugLogger.LogWarning(CoreLibraryDomain.Default, $"Unknown extension for MIME type : {mimeType}");
             return kPlainText;
         }
 
